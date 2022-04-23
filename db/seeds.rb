959a8063dee70de79bb75017dff23c041feab588
Rishat <81845@mail.ru>
2022-04-23 10:54:01 +0300

    Project show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Project.all.each do |project|
+  10.times do
+    Task.create(
+      title: Faker::App.name,
+      describle: Faker::Lorem.paragraph,
+      status: 0,
+      author: User.first,
+      project: project
+    )
+  end
+end
