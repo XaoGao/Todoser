62e662b0e4c110f3e349a4bc5c43df712d836d88
Rishat <81845@mail.ru>
2022-04-30 16:28:12 +0300

    Add user navar item

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,10 @@
 class WelcomeController < ApplicationController
+  before_action :redirect_signed_user
   def index; end
+
+  private
+
+  def redirect_signed_user
+    redirect_to projects_path if user_signed_in?
+  end
 end
