f200cf56fe8df8c1843068c8fc3ed9215cd50567
Rishat <81845@mail.ru>
2022-05-04 22:42:02 +0300

    refactor button_component

--- app/components/button/back_component.rb
+++ app/components/button/back_component.rb
@@ -2,17 +2,10 @@
 
 class Button::BackComponent < Button::BaseComponent
   def initialize(path, option = {})
-    add_default_css_class(option)
     super(I18n.t("button.back"), path, option.merge(icon: "fa-solid fa-arrow-left-long"))
   end
 
-  private
-
-  def add_default_css_class(option)
-    if option[:class].nil?
-      option[:class] = "btn btn-primary"
-    else
-      option[:class] = "#{option[:class]} btn btn-primary"
-    end
+  def default_classes
+    "btn btn-primary"
   end
 end
--- app/components/button/base_component.rb
+++ app/components/button/base_component.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
 class Button::BaseComponent < ViewComponent::Base
-  attr_reader :label, :path, :icon, :option
+  attr_reader :label, :path, :icon, :classes, :option
 
-  def initialize(label, path, option)
+  def initialize(label, path, option = {})
     @label = label
     @path = path
     @icon = option[:icon]
+    @classes = get_classes(option)
     @option = option
   end
 
   alias icon? icon
+
+  def default_classes
+    ""
+  end
+
+  private
+
+  def get_classes(option)
+    if option[:class].nil?
+      option[:class] = default_classes
+    else
+      option[:class] = "#{option[:class]} #{default_classes}"
+    end
+  end
 end
