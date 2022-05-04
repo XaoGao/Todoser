4a3e08b2a63c5c0e7c92c10480aedac058f9e1d3
Rishat <81845@mail.ru>
2022-05-04 22:01:59 +0300

    Add default css style for back_component

--- app/components/button/back_component.rb
+++ app/components/button/back_component.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class Button::BackComponent < Button::BaseComponent
-  def initialize(path, option)
+  def initialize(path, option = {})
+    add_default_css_class(option)
     super(I18n.t("button.back"), path, option.merge(icon: "fa-solid fa-arrow-left-long"))
+  end
+
+  private
+
+  def add_default_css_class(option)
+    if option[:class].nil?
+      option[:class] = "btn btn-primary"
+    else
+      option[:class] = "#{option[:class]} btn btn-primary"
+    end
   end
 end
