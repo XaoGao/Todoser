58718c69e97a5a1df9467e1379fa7017c97630b7
Rishat <81845@mail.ru>
2022-05-04 13:06:11 +0300

    Create task show page

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -16,7 +16,7 @@
 FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
-    last_name  { Faker::Name.last_name   }
+    last_name  { Faker::Name.last_name }
     email { Faker::Internet.email }
     password { "password" }
   end
