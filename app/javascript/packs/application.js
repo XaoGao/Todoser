9be86bdebd8002bcb0c09bfb86045233ae35ac0a
Rishat <81845@mail.ru>
2022-05-05 14:42:08 +0300

    Add sortablejs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,18 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+import "controllers"
 import "bootstrap"
 import "../stylesheet/application.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
+import Sortable from 'sortablejs';
 
 document.addEventListener("turbolinks:load", () => {
-  
+  var els = document.getElementsByClassName('list-group');
+  for (let index = 0; index < els.length; index++) {
+    const element = els[index];
+    var sortable = Sortable.create(element);
+    
+  }
 })
-import "controllers"
+
