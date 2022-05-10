f82bd8d50be206fa4ec2da811fb231a48dd1b8ff
Rishat <81845@mail.ru>
2022-05-10 18:51:55 +0300

    Add dashborad title

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:user) { create(:user) }
 
+  describe "GET dashboard" do
+    context "user is sign in" do
+      before(:each) do
+        sign_in user
+      end
+
+      it "returns http success" do
+        get dashboard_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "user is not sign in" do
+      it "return to a login page" do
+        get dashboard_path
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
