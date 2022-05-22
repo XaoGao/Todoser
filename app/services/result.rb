85b5ec3cf8cd2c805d4c88adda85b9a29fcab337
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-05-22 12:36:45 +0300

    Add: constructor for class Result

--- app/services/result.rb
+++ app/services/result.rb
@@ -1,12 +1,17 @@
-# app/services/result.rb
+class Result
+  attr_reader :status, :data, :error_messages
 
-class Result (status, data, error_messages)
-    
+  def initialize(status, data, error_messages)
+    @status = status
+    @data = data
+    @error_messaages = error_messages
+  end
+
   def success?
-    status = true
+    @status == true
   end
 
   def failure?
-    status = false
+    !status
   end
 end
