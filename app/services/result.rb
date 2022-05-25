afbade8fec5ccc069138ccfc1500f83a58e5a206
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-05-25 10:23:46 +0300

    Fix: XaoGao#4 typo | bool success by default

--- app/services/result.rb
+++ app/services/result.rb
@@ -4,14 +4,14 @@ class Result
   def initialize(status, data, error_messages)
     @status = status
     @data = data
-    @error_messaages = error_messages
+    @error_messages = error_messages
   end
 
   def success?
-    @status == true
+    @status
   end
 
   def failure?
-    !status
+    !@status
   end
 end
