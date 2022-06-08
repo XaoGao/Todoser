8ec65eba7c64795e3b1725849cde6695dd62886a
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-06-08 16:00:24 +0300

    Add: XaoGao/Todoser#54 create MoveService

--- app/services/tasks/move_service.rb
+++ app/services/tasks/move_service.rb
@@ -1,10 +1,5 @@
 module Tasks
   class MoveService < Service
-    def call(task, new_status, new_position)
-      task = Task.find(params[:id])
-
-      tasks_old_column = Task.where("status = ? AND position > ?", Task.statuses[task.status], task.position)
-      tasks_new_column = Task.where("status = ? AND position >= ?", Task.statuses[params[:status]], params[:position])
-    end
+    def call(task, new_status, new_position); end
   end
 end
