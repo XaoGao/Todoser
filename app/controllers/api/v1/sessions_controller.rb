b15bdced739237f9dea1d05fb877ede0a57f2e0f
mrjoshua <mrjoshua@gmail.com>
2022-06-14 18:12:10 +0300

    Refactor create method and spec

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,21 +4,21 @@ module Api
       def create
         if params[:email].nil? || params[:password].nil?
           render status: :bad_request,
-                 json: { error: I18n.t('api.v1.sessions.errors.no_email_and_password') } and return
+                 json: { error: I18n.t("api.v1.sessions.errors.no_email_and_password") } and return
         end
 
         user = User.find_by email: params[:email]
         if user.blank? || !user.enable?
           render status: :bad_request,
-                 json: { error: I18n.t('api.v1.sessions.errors.user_not_found_blocked') } and return
+                 json: { error: I18n.t("api.v1.sessions.errors.user_not_found_blocked") } and return
         end
 
         if !user.valid_password?(params[:password])
           render status: :bad_request,
-                 json: { error: I18n.t('api.v1.sessions.errors.incorrect_email_or_password') } and return
+                 json: { error: I18n.t("api.v1.sessions.errors.incorrect_email_or_password") } and return
         end
 
-        token = JwtService.encode({ id: user.id, email: user.email, username: user.username })
+        token = JwtService.encode(id: user.id, email: user.email, username: user.username)
         render status: :ok, json: { token: token }
       end
     end
--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,39 +1,43 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Api::V1::SessionsController do
   subject { described_class }
 
   let(:user) { create(:user) }
-  let(:token) { JwtService.encode({ id: user.id, email: user.email, username: user.username }) }
+  let(:token) { JwtService.encode(id: user.id, email: user.email, username: user.username) }
 
-  it 'routes correctly' do
-    expect(post: 'api/v1/login').to route_to('api/v1/sessions#create')
+  it "routes correctly" do
+    expect(post: "api/v1/login").to route_to("api/v1/sessions#create")
   end
 
-  describe '#create' do
-    it 'returns users token if email and password are valid' do
-      post :create, params: { email: user.email, password: 'password' }
-      expect(JSON.parse(response.body)['token']).to eq token
+  describe "#create" do
+    it "email and password are valid" do
+      post :create, params: { email: user.email, password: "password" }
+      expect(JSON.parse(response.body)["token"]).to eq token
     end
 
-    it 'returns no email and password error if email is nil' do
-      post :create, params: { password: 'password' }
-      expect(JSON.parse(response.body)['error']).to eq I18n.t('api.v1.sessions.errors.no_email_and_password')
+    it "email is nil or empty" do
+      post :create, params: { password: "password" }
+      expect(JSON.parse(response.body)["error"]).to eq I18n.t("api.v1.sessions.errors.no_email_and_password")
     end
 
-    it 'returns no email and password error if password is nil' do
+    it "password is nil or empty" do
       post :create, params: { email: user.email }
-      expect(JSON.parse(response.body)['error']).to eq I18n.t('api.v1.sessions.errors.no_email_and_password')
+      expect(JSON.parse(response.body)["error"]).to eq I18n.t("api.v1.sessions.errors.no_email_and_password")
     end
 
-    it 'returns user not found or blocked error if user is nil or deleted' do
-      post :create, params: { email: 'wrong@email.com', password: 'password' }
-      expect(JSON.parse(response.body)['error']).to eq I18n.t('api.v1.sessions.errors.user_not_found_blocked')
+    it "user is not found" do
+      post :create, params: { email: "wrong@email.com", password: "password" }
+      expect(JSON.parse(response.body)["error"]).to eq I18n.t("api.v1.sessions.errors.user_not_found_blocked")
     end
 
-    it 'returns incorrect password error if password invalid' do
-      post :create, params: { email: user.email, password: 'wrong_password' }
-      expect(JSON.parse(response.body)['error']).to eq I18n.t('api.v1.sessions.errors.incorrect_email_or_password')
+    it "user is blocked" do
+      # TODO: implements after add delete_at field to users model and enable? method
+    end
+
+    it "password is invalid" do
+      post :create, params: { email: user.email, password: "wrong_password" }
+      expect(JSON.parse(response.body)["error"]).to eq I18n.t("api.v1.sessions.errors.incorrect_email_or_password")
     end
   end
 end
