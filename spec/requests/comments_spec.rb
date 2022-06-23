7e17a270636ffc61ea1a25fef9a0c2952d688233
Alex Smirnov <sas2job@gmail.com>
2022-06-23 15:44:03 +0300

    Fix tests for tests comments#create

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  let(:user) { create(:user) }
-  let(:project) { create(:project, author: user) }
-  let(:task) { create(:task, author: user) }
-
-  describe "POST comments" do
-    let(:comment) { build(:comment, author: user, commentable: task) }
-
+  describe "POST /comments/" do
     context "user is sign in" do
+      let(:user) { create(:user) }
+      let(:project) { create(:project, author: user) }
+      let(:task) { create(:task, author: user) }
+      let(:comment) { build(:comment, author: user, commentable: task) }
+    end
+
       before(:each) do
         sign_in user
       end
 
       it "returns http success" do
-        post comments_path, params: { comment: comment.attributes }
+        post comments_path, params: { commentable_type: comment.class, commentable_id: comment.id }
         expect(response).to have_http_status(:success)
       end
 
       it "create a new comment" do
-        post comments_path, params: { comment: comment.attributes }
-        expect(Comment.first.body).to eq(comment.body)
+        post comments_path, params: { commentable_type: comment.class, commentable_id: comment.id }
+        expect(Comment.first.body).to eq(comment)
       end
-    end
+
 
     context "user is not sign in" do
       it "returns redirect to sign in page" do
-        post comments_path(comment)
+        post comments_path
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to new_user_session_path
       end
