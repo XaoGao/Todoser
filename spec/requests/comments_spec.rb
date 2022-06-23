677b6223a576dd243d02a853b1bbfb0161f51453
Alex Smirnov <sas2job@gmail.com>
2022-06-23 09:55:38 +0300

    Add tests for comments#create

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:project) { create(:project, author: user) }
+  let(:task) { create(:task, author: user) }
+
+  describe "POST comments" do
+    let(:comment) { build(:comment, body: "Comment for task", author: user, task: task) }
+
+    context "user is sign in" do
+      before(:each) do
+        sign_in user
+      end
+
+      it "returns http success" do
+        post comments_path, params: { comment: comment.attributes }
+        expect(response).to have_http_status(:success)
+      end
+
+      it "create a new comment" do
+        post comments_path, params: { comment: comment.attributes }
+        expect(Comment.first.body).to eq(comment.body)
+      end
+    end
+
+    context "user is not sign in" do
+      it "returns redirect to sign in page" do
+        post post comments_path(comment)
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 end
