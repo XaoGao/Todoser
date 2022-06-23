1a07c3ec55f0119252f92376f17c831ac10bed5f
Alex Smirnov <sas2job@gmail.com>
2022-06-23 15:59:03 +0300

    Add action create for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,27 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :load_task!
 
+  def create
+    @comment = @task.find_by(user: current_user)
+    # already exist
+    if @comment.present? && @comment.delete_at.nil?
+      return head :bad_request
+    end
+
+    # enable old favorite
+    if @comment.present? && !@comment.delete_at.nil?
+      @comment.update(delete_at: nil)
+      return head :ok
+    end
+
+    @comment = Comment.new(commentable: @commentable, user: current_user)
+    if @comment.save
+      head :ok
+    else
+      head :bad_request
+    end
+  end
+
   private
 
   def load_task!
