53cffe0100edae2e703de6fddf7a3c6cdbed5fbb
Alex Smirnov <sas2job@gmail.com>
2022-06-24 20:28:25 +0300

    Add comment#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,25 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_task!
+  before_action :load_commentable!
 
   def create
-    @comment = @task.find_by(user: current_user)
-    # already exist
-    if @comment.present? && @comment.delete_at.nil?
-      return head :bad_request
-    end
-
-    # enable old favorite
-    if @comment.present? && !@comment.delete_at.nil?
-      @comment.update(delete_at: nil)
-      return head :ok
-    end
-
     @comment = Comment.new(commentable: @commentable, user: current_user)
+    # already exist
     if @comment.save
-      head :ok
+      return head :bad_request
     else
-      head :bad_request
+      return head :bad_request
     end
   end
 
   private
 
-  def load_task!
+  def load_commentable!
     # TODO Add a check that the task exists
-    @task = Task.find(comment_params[:task_id])
+    @commentable = Task.find(comment_params[:commentable_id])
   end
 
   def comment_params
-    params.require(:comment).permit(:body, :commentable)
+    params.require(:comment).permit(:body, :commentable_type, :commentable_id)
   end
 end
