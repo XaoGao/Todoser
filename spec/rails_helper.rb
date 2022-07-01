6f25072179b1d4d98d18c8a116d08d1de8ee4348
markcodexq <markcodexq@gmail.com>
2022-07-01 14:19:25 +0500

    fix rspec configure for database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,8 +25,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-end
-
-config.before(:suite) do
-  DatabaseCleaner.clean_with(:truncation)
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 end
