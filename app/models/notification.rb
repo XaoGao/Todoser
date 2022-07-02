7b7802222b2c4a0909a448edde6ef41166c743ad
markcodexq <markcodexq@gmail.com>
2022-07-02 15:53:24 +0500

    add model Notification

---
+++ app/models/notification.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: project_members
+#
+#  id         :integer          not null, primary key
+#  body       :string          not null
+#  user_id    :integer          not null
+#  read_at    :datetime         default: nil
+#
+class Notification < ApplicationRecord
+    
+    validates :body, presence: true, length: { in: 1..200 }
+    def read
+        self.update(read_at: DateTime.now)
+    end
+end
