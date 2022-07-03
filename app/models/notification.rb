4d852f838e202de03dfe61e6a951bdea81af3881
markcodexq <markcodexq@gmail.com>
2022-07-03 13:12:28 +0500

    little fix model notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,13 +4,13 @@
 #
 #  id         :integer          not null, primary key
 #  body       :string          not null
-#  user_id    :integer          not null
 #  read_at    :datetime         default: nil
 #
 class Notification < ApplicationRecord
     
     validates :body, presence: true, length: { in: 1..200 }
+    
     def read
-        self.update(read_at: DateTime.now)
+      self.update(read_at: DateTime.now)
     end
 end
