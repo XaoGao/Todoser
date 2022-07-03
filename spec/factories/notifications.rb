b49724f2099909bb3a9518606d5f9516a914a99b
markcodexq <markcodexq@gmail.com>
2022-07-03 13:13:00 +0500

    fix spec factories and models notification

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :notification do
-    
+    body { Faker::Lorem.sentence(word_count: 3) }
+    read_at { nil }
+    association factory: :user
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".read_at" do
+    let(:notification) { create(:notification, read_at: nil) }
+
+    it "read is not nil" do
+
+      expect(notification.read_at).to not_nil
+    end
+  end
 end
