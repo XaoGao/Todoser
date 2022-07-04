3a854cf3eddb9f1324ef443513174842ab3ffd8e
Kylaten <kylatenweb@gmail.com>
2022-07-04 19:58:21 +0300

    Add locale to FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
+#  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  reset_password_token   :string
@@ -14,6 +14,7 @@
 #  updated_at             :datetime         not null
 #  username               :string           default(""), not null
 #  online                 :boolean          default(FALSE)
+#  locale                 :string           default("en"), not null
 #
 FactoryBot.define do
   factory :user do
@@ -23,5 +24,6 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { "password" }
     online { false }
+    locale { "en" }
   end
 end
