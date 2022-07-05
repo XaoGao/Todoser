1f3c4d2bbef491f0e270f3ac697cd6d0bc68722e
markcodexq <markcodexq@gmail.com>
2022-07-05 16:05:46 +0500

    add schema to factories notifications

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id         :integer         not null, primary key
+#  body       :string          not null
+#  read_at    :datetime        default: nil
+#
 FactoryBot.define do
   factory :notification do
     body { Faker::Lorem.sentence(word_count: 3) }
