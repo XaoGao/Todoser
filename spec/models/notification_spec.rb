90bab0e3b1e68dc4d216510732b288db476c5f2d
markcodexq <markcodexq@gmail.com>
2022-07-05 16:05:22 +0500

    Fix model notification_spec

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  describe ".read_at" do
+  describe ".read" do
     let(:notification) { create(:notification, read_at: nil) }
 
     it "read is not nil" do
 
-      expect(notification.read_at).to not_nil
+      expect(notification.read).to not_nil
     end
   end
 end
