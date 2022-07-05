d887405d34442ef44a9d54c7145d4d46cd03140c
markcodexq <markcodexq@gmail.com>
2022-07-05 16:04:46 +0500

    Add description to model notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,16 +1,16 @@
 # == Schema Information
 #
-# Table name: project_members
+# Table name: notifications
 #
 #  id         :integer          not null, primary key
 #  body       :string          not null
 #  read_at    :datetime         default: nil
 #
 class Notification < ApplicationRecord
-    
-    validates :body, presence: true, length: { in: 1..200 }
-    
-    def read
-      self.update(read_at: DateTime.now)
-    end
+
+  validates :body, presence: true, length: { in: 1..200 }
+
+  def read
+    self.update(read_at: DateTime.now)
+  end
 end
