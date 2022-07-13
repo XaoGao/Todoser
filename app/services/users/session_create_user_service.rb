ed1f3d587f5c9f84ce6ad60c5f82833e914b48b3
mrjoshua <mrjoshua@gmail.com>
2022-07-13 14:05:28 +0300

    Refactor call method

--- app/services/users/session_create_user_service.rb
+++ app/services/users/session_create_user_service.rb
@@ -1,17 +1,17 @@
 module Users
   class SessionCreateUserService < Service
-    def call(params = {})
-      if params[:email].nil? || params[:password].nil?
+    def call(email: nil, password: nil)
+      if email.nil? || password.nil?
         return failure(I18n.t("api.v1.sessions.errors.no_email_and_password"))
       end
 
-      user = User.find_by email: params[:email]
+      user = User.find_by email: email
 
       if user.blank? || !user.enable?
         return failure(I18n.t("api.v1.sessions.errors.user_not_found_blocked"))
       end
 
-      if !user.valid_password?(params[:password])
+      if !user.valid_password?(password)
         return failure(I18n.t("api.v1.sessions.errors.incorrect_email_or_password"))
       end
 
