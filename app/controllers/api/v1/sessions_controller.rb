7ccb31b99a808f85f515930389775445b4dcc989
mrjoshua <mrjoshua@gmail.com>
2022-07-14 13:33:02 +0300

    Add SessionCreateUserService and api responders to api sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,24 +2,8 @@ module Api
   module V1
     class SessionsController < Api::ApiController
       def create
-        if params[:email].nil? || params[:password].nil?
-          render status: :bad_request,
-                 json: { error: I18n.t("api.v1.sessions.errors.no_email_and_password") } and return
-        end
-
-        user = User.find_by email: params[:email]
-        if user.blank? || !user.enable?
-          render status: :bad_request,
-                 json: { error: I18n.t("api.v1.sessions.errors.user_not_found_blocked") } and return
-        end
-
-        if !user.valid_password?(params[:password])
-          render status: :bad_request,
-                 json: { error: I18n.t("api.v1.sessions.errors.incorrect_email_or_password") } and return
-        end
-
-        token = JwtService.encode(id: user.id, email: user.email, username: user.username)
-        render status: :ok, json: { token: token }
+        result = Users::SessionCreateUserService.new.call(email: params[:email], password: params[:password])
+        result.success? ? ok(token: result.data) : bad_request(result.error_messages)
       end
     end
   end
