32e9651a3df05daad439a1b23293389abc0f2c55
mrjoshua <mrjoshua@gmail.com>
2022-07-26 17:21:28 +0300

    Add API Tasks controller with show action

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    class TasksController < Api::ApiController
+      before_action :token_authenticate_user
+
+      def show
+        task = Task.find(params[:id])
+        serialized_task = TaskSerializer.new(task).serializable_hash.to_json
+        ok(task: serialized_task)
+      end
+    end
+  end
+end
