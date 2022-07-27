8087b68c717a6a6e140b254dcd6b33589a53642f
Gluuck <ostapenkov74@gmail.com>
2022-07-27 02:39:59 -0700

    change locale & create dropdown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
   def dashboard
     @projects = current_user.member_in_project
   end
+
+  def change_locale
+    if locale.empty? || !I18n.available_locales.include?(locale.to_sym)
+      redirect_to root_path, error: 'Unexpected error'
+    else
+      current_user.update!(locale: locale)
+      redirect_to request.refere
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Todoser
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :en
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
