284c098f8cff695374f73643daba934bdd2d361e
Rishat <81845@mail.ru>
2022-08-06 15:19:16 +0300

    task-show-page

--- spec/models/project_mark_spec.rb
+++ spec/models/project_mark_spec.rb
@@ -12,5 +12,16 @@
 require 'rails_helper'
 
 RSpec.describe ProjectMark, type: :model do
+  describe ".present_title" do
+    let(:project_mark_with_title) { create(:project_mark, title: "example") }
+    let(:project_mark_without_title) { create(:project_mark, title: nil) }
 
+    it "title is present" do
+      expect(project_mark_with_title.present_title).to eq("Example")
+    end
+
+    it "title is empty" do
+      expect(project_mark_without_title.present_title).to eq("-")
+    end
+  end
 end
