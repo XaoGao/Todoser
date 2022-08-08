351486fc832a84b4888d1d1ad4d6a64b18cfb427
mrjoshua <mrjoshua@gmail.com>
2022-08-08 16:53:58 +0300

    Record not found method moved to api tasks controller and api projects controller, add new error message

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -7,10 +7,6 @@ module Api
 
     private
 
-    def record_not_found(exception)
-      bad_request(t("api.v1.tasks.errors.task_not_found"))
-    end
-
     def user_not_authorized
       unauthorized(t("api.errors.unauthorized"))
     end
--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -9,6 +9,12 @@ module Api
         serialized_task = TaskSerializer.new(task).serializable_hash.to_json
         ok(task: serialized_task)
       end
+
+      private
+
+      def record_not_found(exception)
+        bad_request(t("api.v1.tasks.errors.task_not_found"))
+      end
     end
   end
 end
