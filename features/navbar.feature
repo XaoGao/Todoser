158c38a90082effb12c78d93219ab16149f982ac
Vladimir Ostapenko <ostapenkov74@gmail.com>
2022-08-09 19:02:13 +0500

    add contacts step & a little dashboard

--- features/navbar.feature
+++ features/navbar.feature
@@ -1,7 +1,16 @@
 Feature: Navbar
 
-    Scenario: I can click a navbar href
-      Given I logged in and visit my page
-      When I click on navbar href
-      Then I see a page message
-      
+  Scenario: I can click a navbar href about us
+    Given I visit my page
+    When I click on navbar href about us
+    Then I see a page message
+
+  Scenario: I can click a navbar href contacts
+    Given I visit root page
+    When I click on navbar href contacts
+    Then I see a page
+
+  Scenario: I can click a navbar href my dashboard
+    Given I visit on sign in page
+    When I click on navbar href my dashboard
+    Then I see a message
--- features/step_definitions/navbar_steps.rb
+++ features/step_definitions/navbar_steps.rb
@@ -1,11 +1,36 @@
-Given("I logged in and visit my page") do
+Given('I visit my page') do
   visit root_path
 end
 
-When('I click on navbar href') do
+When('I click on navbar href about us') do
   click_on "About us"
 end
 
 Then('I see a page message') do
   expect(page).to have_content("Pangu and the creation of the world")
+end
+
+Given('I visit root page') do
+  visit root_path
+end
+#
+When('I click on navbar href contacts') do
+  click_on "Contacts"
+end
+
+Then('I see a page') do
+  expect(page).to have_content("BRAAIIINS Ipsum")
+end
+
+Given('I can click a navbar href my dashboard') do
+  new_session
+  visit root_path
+end
+#
+When('I click on navbar href my dashboard') do
+  click_on "My dashboard"
+end
+
+Then('I see a message') do
+  expect(page).to have_content("BRAAIIINS Ipsum")
 end
