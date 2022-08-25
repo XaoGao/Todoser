c995479ea1171cd9bf0db7c8597e4083a5c62796
Vladimir Ostapenko <ostapenkov74@gmail.com>
2022-08-25 11:25:26 +0500

    create leave button

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.session_store :cache_store
+  # config.session_store :cache_store
 
   # Configure 'rails notes' to inspect Cucumber files
   config.annotations.register_directories('features')
