552b1275f3ff30fafa44880bf69555fa3495cec4
Alex Smirnov <sas2job@gmail.com>
2022-08-31 09:37:57 +0300

    Add delete_at for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_074810) do
+ActiveRecord::Schema.define(version: 2022_08_30_174326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 2022_08_25_074810) do
     t.string "username", default: "", null: false
     t.boolean "online", default: false
     t.string "locale", default: "en", null: false
+    t.datetime "delete_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
