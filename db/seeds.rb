dc0fab50d7fca6955bac6777137b1dcd46485284
Alex Smirnov <sas2job@gmail.com>
2022-09-02 17:59:31 +0300

    Add user's role to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,14 @@
 end
 
 10.times do |i|
+  roles = [:user, :moderator, :admin]
   User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     username: Faker::Internet.username,
     email: "t#{i}@t.t",
     password: "password",
+    role: "#{roles.sample}",
     locale: "en"
   )
 end
