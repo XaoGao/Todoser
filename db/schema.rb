2528a6987173fb7da2a96d21c63f00e7df2ce07e
Alex Smirnov <sas2job@gmail.com>
2022-09-02 17:44:43 +0300

    Add role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_074810) do
+ActiveRecord::Schema.define(version: 2022_09_02_143132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,8 @@ ActiveRecord::Schema.define(version: 2022_08_25_074810) do
     t.string "username", default: "", null: false
     t.boolean "online", default: false
     t.string "locale", default: "en", null: false
+    t.datetime "delete_at"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
