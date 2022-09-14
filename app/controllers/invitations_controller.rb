df31a89f7e972f92fc094daa4e2f669ebcaaddf5
Alex Smirnov <sas2job@gmail.com>
2022-09-14 22:07:56 +0300

    Add invitation#create

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,10 +2,22 @@ class InvitationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @project = Project.find(params[:project_id])
+    @invitation = Invitation.new(project_id: @project.id, email: params[:email])
 
+    if @invitation.save
+      return head :ok
+    else
+      head :bad_request
+    end
   end
 
   def agree
 
   end
+
+  private
+  # def invitations_params
+  #   params.require(:invitation).permit(:recipient_id, :agree, :delete_at)
+  # end
 end
