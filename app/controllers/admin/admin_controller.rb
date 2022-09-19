bb13c2af727fd5d515123968f358fb1a6ccbcde0
Alex Smirnov <sas2job@gmail.com>
2022-09-19 21:27:45 +0300

    Add authenticate_admin for admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,2 +1,7 @@
 class Admin::AdminController < ApplicationController
+  before_filter :authenticate_admin!
+  # TODO: Подумать как можно проверять роль пользователя внутри метода :authenticate_user! для нужного routes
+  def authenticate_admin!
+    user_signed_in? && current_user.admin?
+  end
 end
