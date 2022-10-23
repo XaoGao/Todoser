3adac9a8251c2e68eabf3030a76b14bb3bf0d0ad
Rishat <81845@mail.ru>
2022-10-23 18:44:33 +0300

    fix registration and new session feature

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -3,13 +3,13 @@ Given("I am on registration page") do
 end
 
 When("I fill all fields and submit form") do
-  fill_in "user_email_registration",	with: "test@test.com"
-  fill_in "user_username_registration",	with: "test_username"
-  fill_in "user_first_name_registration",	with: "test_first_name"
-  fill_in "user_last_name_registration",	with: "test_last_name"
-  fill_in "user_password_registration",	with: "password"
-  fill_in "user_password_confirmation_registration",	with: "password"
-  click_on "submit_registration"
+  fill_in "user_email",	with: "test@test.com"
+  fill_in "user_username",	with: "test_username"
+  fill_in "user_first_name",	with: "test_first_name"
+  fill_in "user_last_name",	with: "test_last_name"
+  fill_in "user_password",	with: "password"
+  fill_in "user_password_confirmation",	with: "password"
+  click_on "user_registration_submit"
 end
 
 Then("I see a welcome message for new user") do
